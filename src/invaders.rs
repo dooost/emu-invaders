1627d75a820bf5ce11956017d9d7a273b7c46501
Basir Doost <basir.doost@prezi.com>
2021-12-12 22:07:09 +0100

    FrameHalf values start from 1, not 0, runs beautifully now!

--- src/invaders.rs
+++ src/invaders.rs
@@ -1,13 +1,13 @@
 use crate::display::Display;
 use emu_8080::emulator::{IOHandler, State8080};
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 const CYCLES_PER_FRAME: u64 = 4_000_000 / 60;
 
 #[derive(Copy, Clone, PartialEq)]
 #[repr(u16)]
 pub enum FrameHalf {
-    Top,
+    Top = 1,
     Bottom,
 }
 
@@ -76,12 +76,6 @@ impl Invaders {
     }
 
     pub fn run(mut self) {
-        // let mut self = self;
-
-        // let mut last_time = None;
-        // let mut next_interrupt_time = None;
-        // let mut next_interrupt_kind = FrameHalf::Top;
-
         loop {
             self = self.frame();
         }
